065e8f266d70da5f6af06f7471183ff6fe40024f
PyAdminRU <zh.vitaliy92@gmail.com>
2025-08-20 23:21:46 +0300

    Добавлена документация к проекту. Добавлено README.md

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import time
+
 from loguru import logger
 
 from get_regions_tree import get_regions_tree
 from keys import OAuth
 from regions import get_wordstat_by_regions
 from regions_utils import pretty_regions
+
 
 def main():
     """
@@ -49,6 +51,7 @@ def main():
         else:
             print(f"❌ Не удалось получить данные для '{keyword}'")
         time.sleep(1)  # чтобы не превысить лимиты
+
 
 if __name__ == "__main__":
     main()
